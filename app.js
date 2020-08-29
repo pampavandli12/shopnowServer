11cadc14fcae24944e7e9e53eabdd9601b6c6bc8
Pampapathi <pampa.vandli@gmail.com>
2020-08-29 19:16:35 +0530

    implimented twilio sms verificaion for admin

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+const adminController = require("./controllers/admin");
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>This is from server</h1>");
 });
-
+adminController(app);
 app.listen(port, () => console.log(`App is listening ap port: ${port}`));

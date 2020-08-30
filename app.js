6a77299af5ac2f9518548dc36272b3e8faad9db4
Pampapathi <pampa.vandli@gmail.com>
2020-08-30 14:38:38 +0530

    mongodb connection established

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+// Require modules
 const express = require("express");
+const mongoose = require("mongoose");
+// Load .env variables
+require("dotenv").config();
 
-const adminController = require("./controllers/admin");
+// Require controllers
+const AdminController = require("./controllers/admin");
 const Authenticate = require("./controllers/authenticate");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/user", Authenticate);
 
 const port = process.env.PORT || 3000;
+
+// DB Connections
+mongoose.connect(process.env.MONGODB_URL, (err) => {
+  if (!err) {
+    throw err;
+  }
+  console.log("Successfully connected to DB");
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>This is from server</h1>");
 });
-adminController(app);
+app.use("/user", Authenticate);
+app.use("/admin", AdminController);
+
 app.listen(port, () => console.log(`App is listening ap port: ${port}`));
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,23 +1,24 @@
-require("dotenv").config();
 const phoneNumber = process.env.PHONE_NUMBER;
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const adminNumber = process.env.ADMIN_NUMBER;
 const client = require("twilio")(accountSid, authToken);
+const express = require("express");
+const Router = express.Router();
 
-module.exports = (app) => {
-  app.get("/admin/verification", (req, res) => {
-    const OTP = Math.floor(100000 + Math.random() * 900000);
-    client.messages
-      .create({
-        body: `This is the OTP for admin from shopnow: ${OTP}`,
-        from: phoneNumber,
-        to: adminNumber,
-      })
-      .then((message) => {
-        console.log(message);
-        res.send("OTP send successfully");
-      })
-      .catch((err) => console.log(err));
-  });
-};
+Router.get("/verification", (req, res) => {
+  const OTP = Math.floor(100000 + Math.random() * 900000);
+  client.messages
+    .create({
+      body: `This is the OTP for admin from shopnow: ${OTP}`,
+      from: phoneNumber,
+      to: adminNumber,
+    })
+    .then((message) => {
+      console.log(message);
+      res.send("OTP send successfully");
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = Router;
